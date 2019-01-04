33fa5ada6a289da42413bf49311a1800ff008abf
ruanpienaar <ruan800@gmail.com>
2019-01-04 08:38:57 +0000

    Start helper

--- start-dev.sh
+++ start-dev.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 cd `dirname $0`
-exec erl -sname hawk -config $PWD/config/sys.config -pa _build/default/lib/*/ebin -boot start_sasl -setcookie hawk -proto_dist hawk_tcp -r 
- -hidden
+exec erl -sname hawk -config $PWD/config/sys.config -pa _build/default/lib/*/ebin -boot start_sasl -setcookie hawk -proto_dist hawk_tcp \
+-eval 'ok = application:start(hawk).' -hidden
 
 # NOT working :(
 #ERL_FLAGS="-sname hawk -config config/sys.config -pa _build/default/lib/*/ebin -boot start_sasl -setcookie hawk -proto_dist hawk_tcp" ./rebar3 shell
